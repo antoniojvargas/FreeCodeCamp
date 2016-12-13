715529a178cc952f733e1cf926cb0066de75dc65
Antonio Vargas <toyoyo600@gmail.com>
2016-12-12 22:21:21 -0400

    Create Repeat_a_string_repeat_a_string.js

---
+++ BasicAlgorithmScripting/Repeat_a_string_repeat_a_string.js
@@ -0,0 +1,9 @@
+	function repeatStringNumTimes(str, num) {
+	  // repeat after me
+	  var strTarget ="";
+	  for (var i=0; i < num; i++) {
+		  strTarget = strTarget + str;
+	  }
+	  
+	  return strTarget;
+	}
