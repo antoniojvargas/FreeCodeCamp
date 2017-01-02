8749e9ba32843c1fa7baa91aa53005e79376d344
Antonio Vargas <toyoyo600@gmail.com>
2017-01-02 14:50:40 -0400

    Create Wherefore_art_thou.js

---
+++ BasicAlgorithmScripting/Wherefore_art_thou.js
@@ -0,0 +1,14 @@
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var srcKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(function (obj) {
+    for(var i = 0; i < srcKeys.length; i++) {
+      if(!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
