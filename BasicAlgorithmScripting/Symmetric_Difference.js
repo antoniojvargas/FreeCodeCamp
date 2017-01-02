8028e6934b9ccb55597abe9bd3fe07c150aad03c
Antonio Vargas <toyoyo600@gmail.com>
2017-01-02 15:58:42 -0400

    Create Symmetric_Difference.js

---
+++ BasicAlgorithmScripting/Symmetric_Difference.js
@@ -0,0 +1,23 @@
+function sym(args) {
+  args = Array.prototype.slice.call(arguments);
+
+  function symDiff(arrayOne, arrayTwo) {
+        var result = [];
+
+        arrayOne.forEach(function(item) {
+            if (arrayTwo.indexOf(item) < 0 && result.indexOf(item) < 0) {
+                result.push(item);
+            }
+        });
+
+        arrayTwo.forEach(function(item) {
+            if (arrayOne.indexOf(item) < 0 && result.indexOf(item) < 0) {
+                result.push(item);
+            }
+        });
+
+        return result;
+    }
+
+    return args.reduce(symDiff);
+}
