0d662714ece306b6991d58a65bc46bcd0ee46e6c
Antonio Vargas <toyoyo600@gmail.com>
2017-01-02 14:58:34 -0400

    Create Boo_who.js

---
+++ BasicAlgorithmScripting/Boo_who.js
@@ -0,0 +1,11 @@
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  //console.log();
+  var type = typeof(bool);
+  if(type === "boolean"){
+    return true;
+  
+  } else {
+    return false;
+  }
+}
